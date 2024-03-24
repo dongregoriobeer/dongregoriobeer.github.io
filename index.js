f59ed08dd6e924d2988d8d80af2d91a3e3008dae
dongregoriobeer <159393944+dongregoriobeer@users.noreply.github.com>
2024-03-24 17:55:07 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const Pass = process.env.PASS;
+document.addEventListener("DOMContentLoaded", Comprobacion())
+
+Comprobacion() {
+    while(localStorage.getItem("DonPass") == "" || localStorage.getItem("DonPass") == null){
+    window.location.href = "https://dongregoriobeer.github.io/index.html";
+ }
+
+    if(localStorage.getItem("DonPass") != Pass){
+    window.location.href = "https://google.com/";
+ }
+}
