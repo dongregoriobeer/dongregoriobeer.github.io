edb5202603c0c3b1308035111405c8134e18d851
dongregoriobeer <159393944+dongregoriobeer@users.noreply.github.com>
2024-03-24 18:58:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Pass = 5277
-document.addEventListener("DOMContentLoaded", Comprobacion())
 
+document.addEventListener("DOMContentLoaded", Comprobacion())
 function Comprobacion() {
     while(localStorage.getItem("DonPass") == "" || localStorage.getItem("DonPass") == null){
     window.location.href = "https://dongregoriobeer.github.io/index.html";
@@ -9,4 +9,10 @@ function Comprobacion() {
     if(localStorage.getItem("DonPass") != Pass){
     window.location.href = "https://google.com/";
  }
+}
+
+let Ubicaciones = ["La Madrid", "La Madrid", "La Madrid", "La Madrid", "La Madrid", "La Madrid", "La Madrid"];
+
+function Ubicacion(Barril) {
+  alert(Ubicaciones[Barril - 1]);
 }
